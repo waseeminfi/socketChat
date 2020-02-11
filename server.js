8033d14afa877f105c7888eae8328533304d0214
waseeminfi <waseem.ahmad@infinitylabs.in>
2020-02-11 11:47:48 +0530

    added cors

--- server.js
+++ server.js
@@ -55,6 +55,8 @@ const {
 // see: http://stackoverflow.com/questions/7067966/how-to-allow-cors-in-express-nodejs
 var allowCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
         res.send(200);
